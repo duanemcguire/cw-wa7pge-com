02f2cb37099d9a97b2869182b7f5a4ab834a0d15
Duane McGuire <biz@duanemcguire.com>
2025-07-02 11:43:27 -0600

    temp

--- api/app/routes/phrases/phrases_controller.py
+++ api/app/routes/phrases/phrases_controller.py
@@ -11,7 +11,7 @@ def songtitles():
     current_file = os.path.abspath(__file__)
     current_dir = os.path.dirname(current_file)
     os.chdir(current_dir)
-    TEXT_FOLDER = "text_files"
+    TEXT_FOLDER = "text_files/songs"
     files = sorted([f for f in os.listdir(TEXT_FOLDER)])
     selected_file = request.form.get('filename')
     wpm = request.form.get('wpm')
@@ -44,20 +44,26 @@ def song_titles_sending():
     os.chdir(current_dir)
     TEXT_FOLDER = "text_files"
 
-    files = sorted([f for f in os.listdir(TEXT_FOLDER)])
-    selected_file = request.form.get('filename')
+    categories = sorted([f for f in os.listdir(TEXT_FOLDER)])
+    selected_category = request.form.get('category')
 
-    line = None
+    if selected_category:
+        line = None
 
-    if selected_file:
-        file_path = os.path.join(TEXT_FOLDER, selected_file)
-        try:
-            with open(file_path, 'r') as f:
-                lines = f.readlines()
-                if lines:
-                    line = random.choice(lines).strip()
-        except Exception as e:
-            line = f"Error reading file: {e}"
+        if selected_file:
+            file_path = os.path.join(TEXT_FOLDER, selected_file)
+            try:
+                with open(file_path, 'r') as f:
+                    lines = f.readlines()
+                    if lines:
+                        line = random.choice(lines).strip()
+            except Exception as e:
+                line = f"Error reading file: {e}"
 
-    return render_template('phrases/song-titles-sending.html', files=files, selected_file=selected_file, line=line)
+    return render_template('phrases/song-titles-sending.html', 
+        categories=categories,
+        selected_category=selected_category,
+        files=files, 
+        selected_file=selected_file, 
+        line=line)
 

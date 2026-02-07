5e1213fa42ce2086854493c19068e28641a4c57d
Duane McGuire <biz@duanemcguire.com>
2026-02-06 20:11:15 -0700

    User Guide, Phrase Copy Tool

--- api/app/routes/main/main_controller.py
+++ api/app/routes/main/main_controller.py
@@ -5,3 +5,8 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/user-guide')
+@main.route('/user-guide/')
+def user_guide():
+    return render_template('user-guide.html')
